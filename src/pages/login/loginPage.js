14bb0b97409708e14fb0061b623ef3542b98c740
Nick <49137044+Nicholas-Castle@users.noreply.github.com>
2020-06-09 20:30:01 -0700

    Style: styling login in page

--- src/pages/login/loginPage.js
+++ src/pages/login/loginPage.js
@@ -6,8 +6,24 @@ import './loginPage.styles.scss'
 const LoginPage = () => {
   return (
     <div className='login-container'>
-      Hello
-      <LoginPanel />
+      <div className='color-strip-top'></div>
+      <div className='panel-container'>
+      <h1 className='question-mark'>?</h1>
+      <h1 className='heading'>Would You Rather</h1>
+      <p className='copy'>Select a account to sign in</p>
+      <form>
+        <label>
+        <select required={true}>
+          <option value='user1'>user1</option>
+          <option value='user2'>user2</option>
+          <option value='user3'>user3</option>
+        </select>
+        </label>
+      </form>
+      <button type='submit' className='login-btn'>SIGN IN</button>
+      
+  </div>
+   
     </div>
   )
 }
